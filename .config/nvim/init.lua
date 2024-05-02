bb5a529d71dc14fe7818a0fbfe7622dfdfdf5d69
Bartosz Chowaniak <chowaniak.bartosz@gmail.com>
2024-05-02 20:45:51 +0200

    break up neovim config into smaller files and add lualine and neotree plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,3 @@
--- tab = 2 spaces
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
-vim.g.mapleader = " "
-
 -- Add plugin manager - Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -20,28 +12,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
+require("vim-options")
+require("lazy").setup("plugins")
 
-require("lazy").setup(plugins, opts)
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local config = require("nvim-treesitter.configs")
-
-config.setup({
-  ensure_installed = {"lua", "javascript"},
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
-require("gruvbox").setup()
-vim.cmd.colorscheme "gruvbox"
